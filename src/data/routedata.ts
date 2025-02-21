b3d99f4a0a14d97ca822b6f9ca345e77a95106ee
Naved <naved10055@gmail.com>
2025-02-21 21:01:35 +0530

    css  learn

---
+++ src/data/routedata.ts
@@ -0,0 +1,22 @@
+export const routeData = [
+  {
+    id: 1,
+    title: "Styling with utility classes",
+    href: "/styling-with-utility-classes"
+  },
+  {
+    id: 2,
+    title: "Styling with utility classes",
+    href: "/styling-with-utility-classes"
+  },
+  {
+    id: 3,
+    title: "Styling with utility classes",
+    href: "/styling-with-utility-classes"
+  },
+  {
+    id: 4,
+    title: "Styling with utility classes",
+    href: "/styling-with-utility-classes"
+  },
+];
